05973e8d0d3bf47a36fb39d9ab81cd64d10dafd7
unknown <Administrador@ESDF106993242.anac.gov.br>
2023-01-17 20:27:27 -0300

    Exercicio de JPA

---
+++ cadastroAluno/src/main/java/cadastroAluno/cadastroAluno/model/Aluno.java
@@ -0,0 +1,51 @@
+package cadastroAluno.cadastroAluno.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+
+@Entity
+public class Aluno {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
+	
+	private String nome;
+	
+	private int idade;
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
+	
+	
+	
+}

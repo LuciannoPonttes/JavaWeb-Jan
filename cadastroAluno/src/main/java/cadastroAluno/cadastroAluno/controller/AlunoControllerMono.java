84bb91849c2c3693f107468281f9db46af28f54a
unknown <Administrador@ESDF106993242.anac.gov.br>
2023-01-20 20:21:01 -0300

    Exercicio de JPA e Spring MVc v7

--- cadastroAluno/src/main/java/cadastroAluno/cadastroAluno/controller/AlunoControllerMono.java
+++ cadastroAluno/src/main/java/cadastroAluno/cadastroAluno/controller/AlunoControllerMono.java
@@ -2,7 +2,10 @@ package cadastroAluno.cadastroAluno.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -68,6 +71,30 @@ public class AlunoControllerMono {
 		
 		alunoRepository.delete(aluno);
 		
+		return "redirect:/listaDeAlunos";
+	}
+	
+	@RequestMapping(value ="/{codigo}", method = RequestMethod.GET)
+	public ModelAndView editarAlunoMetodo(@PathVariable("codigo") long codigo) {
+		Aluno aluno = alunoRepository.findById(codigo);
+		ModelAndView modelAndViewAluno = new ModelAndView("editarAluno");
+		modelAndViewAluno.addObject("aluno", aluno);
+		
+		
+		return modelAndViewAluno;
+		
+	}
+	
+	
+	@RequestMapping(value ="/{codigo}", method = RequestMethod.POST)
+	public String editarAlunoPost(Aluno aluno) {
+		
+		
+		AlunoService alunoService = new AlunoService();
+		
+		aluno.setValorBolsa(alunoService.calcularBolsa(aluno.getQtdHora(), aluno.getValorHora()));
+		
+		alunoRepository.save(aluno);
 		return "redirect:/listaDeAlunos";
 	}
 	
